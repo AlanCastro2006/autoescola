c7ed86b7497b7d72301098f1f56f0ffb4db5612f
AlanCastro2006 <166616369+AlanCastro2006@users.noreply.github.com>
2024-12-21 09:59:33 -0300

    Adicionando botão WhatsApp flutuante
    Adicionando um link de WhatsApp em formato de um botão flutuante com animação em todas as telas através do layout

--- autoescola/resources/views/layouts/layout.blade.php
+++ autoescola/resources/views/layouts/layout.blade.php
@@ -122,6 +122,12 @@
 
     <main>
         @yield('content')
+        <a href="https://wa.me/SEU_NUMERO" target="_blank" class="whatsapp-button">
+    <div class="pulse-ring"></div>
+    <img src="https://upload.wikimedia.org/wikipedia/commons/6/6b/WhatsApp.svg" alt="WhatsApp" />
+
+</a>
+
     </main>
 
     {{-- * Footer * --}}
